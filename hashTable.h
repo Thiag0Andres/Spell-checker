e94a74a7d656d13f0a70b7b116680e4cc42c3bf0
Thiag0Andres <thiagopalacios@eng.ci.ufpb.br>
2020-03-24 21:56:06 -0300

    .

--- hashTable.h
+++ hashTable.h
@@ -1,4 +1,26 @@
 #ifndef HASHTABLE_H
-    
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <ctype.h>
+
+#define MAX_BUCKETS 
+#define MAX_STRING
+
+struct node{
+    struct Node* next;
+    char *key;
+};
+
+
+typedef struct HashTable{
+    struct Node* buckets[MAX_BUCKETS];
+    int numElements[MAX_BUCKETS];
+}HashTable;
+
+extern 
+
 
 #endif // !HASHTABLE_H

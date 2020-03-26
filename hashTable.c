62397b07ab4f30163b0ec677ce02ea737464c8d5
Thiag0Andres <thiagopalacios@eng.ci.ufpb.br>
2020-03-26 19:01:31 -0300

    .

--- hashTable.c
+++ hashTable.c
@@ -1,16 +1,28 @@
 #include "hashTable.h"
 
-
 HashTable* createHashTable() {
+    HashTable *h = (HashTable*) malloc (sizeof(HashTable));
 
+    for(int i=0; i < MAX_BUCKETS; i++){
+        h->buckets[i] = NULL;
+        h->numElements[i] = 0;
+    }
+
+    return h;
 };
 
 unsigned Hash(char *key) {
+    unsigned hash;
 
+    for(hash=0; *key != '\0'; key++){
+        hash = *key + 31 * hash;
+    }
+
+    return hash % MAX_BUCKETS;
 };
 
 void Insert(HashTable *h, char *key) {
-
+    
 };
 
 bool Search(HashTable *h, char *key) {
